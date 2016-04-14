9353db41206847093a31e8742a611b4b2c5567e6
FreshLeaf8865 <FreshLeaf8865@users.noreply.github.com>
2016-04-14 09:34:13 +0200

    Search result page update

--- application/views/myViews/view2.php
+++ application/views/myViews/view2.php
@@ -18,55 +18,6 @@
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
     <script src="<?php echo $this->config->base_url(); ?>js/script.js"></script>
-    <style>
-
-        tr td ul li small font{display :none;}
-        dt small{display:none;}
-        dl dt a {
-             color: #2d95f4;
-             font-size: 17px;
-             text-decoration: none;
-        }
-        dl dt a span {
-             color: #2d95f4 !important;
-             font-size: 17px !important;
-             text-decoration: none !important;
-             background:transparent !important;
-             font-weight: lighter !important;
-        }
-        dl dd span {
-             color: #000000 !important;
-             text-decoration: none !important;
-             background:transparent !important;
-             font-weight: lighter !important;
-        }
-        dd {
-             float: left;
-             margin: 0;
-             width: 100%;
-        }
-        dd ul {padding:0px;}
-             
-        dt {
-             color: #fff;
-        }
-        dl {
-             float: left;
-             margin: 18px 0 0 !important;
-             width: 60%;
-        }
-        dd table {
-             width: 100% !important;
-        }
-        dd * {
-             font-size: 14px !important;
-             margin-left: 12px;
-             margin-top: 5px !important;
-        }
-        dd tbody tr:last-child{display:none;}
-        .pagination{width:100%;float:left;text-align:center; margin: 30px 0;}
-        .pagination a{color:black !important;font-weight:bold;text-decoration:none;}
-    </style>
 </head>
 <body>
     <div class="header-wrapper">
@@ -99,92 +50,103 @@
              </div>
         </div>
     </div>
-    <div id="showresults"></div>
-		<div class="results">
-			<?php
-				if(isset($_REQUEST['button']))
-				{
-					$value=$_REQUEST['search'];
-					//echo $data = file_get_contents('https://ahmia.fi/search/?q='.$value.'');
-
-					// Retrieve the DOM from a given URL
-					$html = file_get_html('http://xmh57jrzrnw6insl.onion.link/5dc02cc7lc/search.cgi?q='.$value.'&cmd=Search!');
-					 //$ol=$html->find('gsc-webResult'); 
-					 $ol=$html->find('dl'); 
-					if(empty($ol))
-					{
-			?>
-				<div style="color:#0081f9;font-size: 17px;margin-left: 9px;margin-top: 15px;">
-					Sorry, couldn't find results for
-					<span style="color:#000"><?php echo $value ?> </span> 
-					.Please try again with a different search.
-				</div>
-				<?php
-					}
-					else
-					{
-						foreach($ol as $e)
-							echo $e;
-					}
-						if(!empty($ol))
-					{
-				?> 
-				    <div class="pagination">
-				   <?php
-						//pagination coding
-						$i=1;
-						for($i=1;$i<=10;$i++)
-						{
-					?>
-						<a href="<?php echo $this->config->base_url();?>index.php/Welcome2/results?page=<?php echo $i ;?>&value=<?php echo $value ;?>"><?php echo $i;?></a>
-					<?php
-						}
-					?>
-					 </div>
-					<?php
-					}
-
-                 }
-                 else
-                 {
-						$page_no=$_GET['page'];
-						$value=$_GET['value'];
-
-						$html = file_get_html('http://xmh57jrzrnw6insl.onion.link/5dc02cc7lc/search.cgi?cmd=Search!&np='.$page_no.'&q='.$value.'');
-							 //$ol=$html->find('gsc-webResult'); 
-							 $ol=$html->find('dl'); 
-
-								foreach($ol as $e)
-								echo $e;
-								
-								?><div class="pagination"><?php
-								$i=1;
-						for($i=1;$i<=10;$i++)
-							{
-						?>
-						
-								<a href="<?php echo $this->config->base_url();?>index.php/Welcome2/results?page=<?php echo $i ;?>&value=<?php echo $value ;?>">
-							<?php echo $i;?></a>
-						<?php
-							}
-					
-	}
-						?>
-					</div>
-		</div>
-                 <div class="bottom-blank"></div>
 </div>
+ <div class="results results_section">
+<div class="results">
+    <?php
+    if(isset($_REQUEST['button']))
+    {
+       $value=$_REQUEST['search'];
+       //echo $data = file_get_contents('https://ahmia.fi/search/?q='.$value.'');
+       // Retrieve the DOM from a given URL
+       $html = file_get_html('http://xmh57jrzrnw6insl.onion.link/5dc02cc7lc/search.cgi?q='.$value.'&cmd=Search!');
+       //$ol=$html->find('gsc-webResult');
+       $ol=$html->find('dl');
+       if(empty($ol))
+       {
+    ?>
+       <div style="margin-left: 9px;margin-top: 15px;">
+            Sorry, couldn't find results for
+            <span style="color:#000"><?php echo $value ?> </span>
+            .Please try again with a different search.
+       </div>
+    <?php
+        }
+        else
+        {
+           foreach($ol as $e)
+           echo $e;
+        }
+        if(!empty($ol))
+        {
+    ?>
+            <div class="page_go">
+    <?php
+         //pagination coding
+                 $i=1;
+    ?>
+            <a href="" style="color:#7ED321 !important;">1</a>
+    <?php
+                 for($i=2;$i<=10;$i++)
+                 {
+    ?>
+                    <a href="<?php echo $this->config->base_url();?>index.php/Welcome2/results?page=<?php echo $i ;?>&value=<?php echo $value ;?>"><?php echo $i;?></a>
+    <?php
+                 }
+    ?>
+            </div>
+    <?php
+         }
                  
+    }
+    else
+    {
+        $page_no=$_GET['page'];
+        $value=$_GET['value'];
+        $html = file_get_html('http://xmh57jrzrnw6insl.onion.link/5dc02cc7lc/search.cgi?cmd=Search!&np='.$page_no.'&q='.$value.'');
+        //$ol=$html->find('gsc-webResult');
+        $ol=$html->find('dl');
+        foreach($ol as $e)
+            echo $e;
+ 
+    ?>
+            <div class="page_go">
+    <?php
+                    for($i=1;$i<=10;$i++)
+                    {
+                        if( $page_no == $i )
+                        {
+                 ?>
+                            <a href="<?php echo $this->config->base_url();?>index.php/Welcome2/results?page=<?php echo $i ;?>&value=<?php echo $value ;?>" style="color:#7ED321;">
+                            <?php echo $i;?></a><?php
+                        }
+                        else
+                        {
+                            ?><a href="<?php echo $this->config->base_url();?>index.php/Welcome2/results?page=<?php echo $i ;?>&value=<?php echo $value ;?>">
+                            <?php echo $i;?></a><?php
+                        }
+    ?>
                  
-                 <div class="bottom-link">
-                 <ul>
-                 <li>Copyright 2016 Mother Inc. All rights reserved.</li>
-                 <li><a href="<?php echo base_url();?>index.php/terms">Term and conditions -</a></li>
-                 <li><a href="<?php echo base_url();?>index.php/privacy">Privacy Policy - </a></li>
-                 <li><a href="*">Feedbacks - </a></li>
-                 <li><a href="*">Donations</a></li>
-                 </ul>
-                 <div class="footer-mother">Mother</div>
-                 </div>
-                 </body>
-                 </html>
+    <?php
+                    }
+          }          
+    
+    ?>
+            </div>
+                 
+</div>
+</div>
+<div class="bottom-section">
+<div class="bottom-link">
+    <ul>
+        <li>Copyright 2016 Mother Inc. All rights reserved.</li>
+        <li><a href="<?php echo $this->config->base_url();?>index.php/terms">Term and conditions -</a></li>
+        <li><a href="<?php echo $this->config->base_url();?>index.php/privacy">Privacy Policy - </a></li>
+        <li><a href="*">Feedbacks - </a></li>
+        <li><a href="*">Donations</a></li>
+    </ul>
+    <div class="footer-mother">Mother</div>
+</div>
+</div>
+</body>
+</html>
